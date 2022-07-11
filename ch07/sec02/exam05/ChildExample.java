ba6d5808830e670ac04942eaa021a48dea816f98
mango0422 <tom990422@gmail.com>
2022-07-11 19:33:32 +0900

    2022-07-11_before_dinner

--- ch07/sec02/exam05/ChildExample.java
+++ ch07/sec02/exam05/ChildExample.java
@@ -1,5 +1,19 @@
 package ch07.sec02.exam05;
 
 public class ChildExample {
-    
+    public static void main(String[] args){
+        Parent parent = new Child(); // 자동 타입 변환
+        parent.field1 = "data1";
+        parent.method1();
+        parent.method2();
+        /*
+        불가능한 것들..
+         parent.field2 = "data2";
+         parent.method3();
+        */
+        
+         Child child = (Child) parent; // 강제 타입 변환
+         child.field2 = "yyy";
+         child.method3();
+    }
 }
