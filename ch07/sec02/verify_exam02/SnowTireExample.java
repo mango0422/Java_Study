0c3b20d8497567b7230f138e99eca3a373303c8b
mango0422 <tom990422@gmail.com>
2022-07-13 09:50:09 +0900

    2022-07-13_AM_LV1

---
+++ ch07/sec02/verify_exam02/SnowTireExample.java
@@ -0,0 +1,11 @@
+package ch07.sec02.verify_exam02;
+
+public class SnowTireExample {
+    public static void main(String[] args){
+        SnowTire snowTire = new SnowTire();
+        Tire tire = new Tire();
+
+        snowTire.run();
+        tire.run();
+    }
+}
