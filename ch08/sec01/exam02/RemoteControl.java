4109a75671845130d86d5a9e768fccffa2b8108b
mango0422 <tom990422@gmail.com>
2022-07-17 17:48:44 +0900

    2022-07-15

---
+++ ch08/sec01/exam02/RemoteControl.java
@@ -0,0 +1,10 @@
+package ch08.sec01.exam02;
+
+public interface RemoteControl{
+    public int MAX_VOLUME = 10;
+    public int MIN_VOLUME = 0;
+}
+
+/*
+ * RemoteControl 인터페이스에 MAX_VOLUME과 MIN_VOLUME을 선언한 모습이다.
+ */
