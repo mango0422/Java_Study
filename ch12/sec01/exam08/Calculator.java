e4b16a68bb46c97e456bd556bf1774ce3127deb4
mango0422 <tom990422@gmail.com>
2022-08-24 18:00:52 +0900

    2022-08-24_IN_CAFE

--- ch12/sec01/exam08/Calculator.java
+++ ch12/sec01/exam08/Calculator.java
@@ -8,6 +8,10 @@ public class Calculator{
     }
 
     public synchronized void setMemory(int memory){
-        
+        this.memory = memory;
+        try{
+            Thread.sleep(2000);
+        } catch(InterruptedException e){}
+        System.out.println(Thread.currentThread().getName() + ": " + this.memory);
     }
 }
---
+++ ch12/sec02/exam03/InterruptExample.java
@@ -0,0 +1,11 @@
+package ch12.sec02.exam03;
+
+public class InterruptExample {
+    public static void main(String[] args){
+        Thread thread = new PrintThread2();
+        thread.start();
+
+        try{ Thread.sleep(1000); } catch(InterruptedException e){}
+        thread.interrupt(); // 스레드를 종료하기 위해 InterruptedException을 발생시킴           
+    }
+}
