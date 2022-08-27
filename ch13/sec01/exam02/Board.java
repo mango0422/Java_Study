49780dec25064eed49b441e004203c83e268783c
mango0422 <tom990422@gmail.com>
2022-08-27 16:26:53 +0900

    2022-08-26

---
+++ ch13/sec01/exam02/Board.java
@@ -0,0 +1,13 @@
+package ch13.sec01.exam02;
+
+public class Board {
+    String subject;
+    String content;
+    String writer;
+
+    public Board(String subject, String contet, String writer){
+        this.subject = subject;
+        this.content = contet;
+        this.writer = writer;
+    }
+}
